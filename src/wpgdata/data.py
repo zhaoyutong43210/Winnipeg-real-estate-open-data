624c97927b1dfb1d619b93f8d6ece8fa8d49a6e1
Yutong Zhao <yutong.zhao@algolux.com>
2023-08-28 13:24:11 -0400

    pdf read update

--- src/wpgdata/data.py
+++ src/wpgdata/data.py
@@ -2,64 +2,80 @@ import numpy as np
 import urllib
 import pathlib
 import logging
+import pdfplumber
+# from .utils import
 
-logger = logging.getLogger('mylogger')
+logging.basicConfig(format=' %(asctime)s - %(levelname)s - %(message)s')
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
 
 class wpgdata_cfg():
     '''The configurator for Winnipeg Real Estate data.'''
+    __years__ = [2012, 2014, 2016, 2018, 2021]  # The years that release the salebooks.
+    __regions__ = np.arange(1, 11)              # The salebooks in different regions.
+    __typedict__ = {'house': 'market_region_', 'condo': 'condominium'}
+
     def __init__(self):
 
-        self.data_save_path = pathlib.Path().absolute() / "Winnipeg_real_estate_open_data"
-        self.data_auto_load()
+        self.data_save_path = pathlib.Path().absolute().parents[1] / "Winnipeg_real_estate_open_data"
+        # self.data_auto_load()
 
     def auto_download(self):
-        '''Download all file needed.'''
-        file_dict = self.full_all_urls()
+        '''Download all (PDF) files needed.'''
+        file_dict = self.full_salebooks_urls()
 
         for key in file_dict:
             file_list = file_dict[key]
             for file in file_list:
                 self._download_file(file, save_sub_path = str(key))
 
-    def full_all_urls(self):
+    def full_salebooks_urls(self):
         '''Get a dict of list with key = year.'''
         result = {}
-        years = [2012, 2014, 2016, 2018, 2021]
-        regions = np.arange(1,11)
-        for year in years:
-            url_list =[]
-            for region in regions:
+        for year in self.__years__:
+            url_list = []
+            for region in self.__regions__:
                 bookurl = self.url_gen(type='house', year=year, region=region)
                 url_list.append(bookurl)
             bookurl = self.url_gen(type='condo', year=year, region=region)
             url_list.append(bookurl)
             result.update({year: url_list})
         return result
-    
+
     def url_gen(self, type='house', year=2012, region=1):
         '''
         Generate the full url of the required.
-        
+
         This function is subject to change over time, depends on the winnipeg city website.
         '''
 
-        typedict = {'house' : 'market_region_', 'condo' : 'condominium'}
         extention = '.pdf'
         base_url = 'https://assessment.winnipeg.ca/AsmtTax/pdfs/SelfService/'
-        key_word= 'sales_book_'
+        key_word = 'sales_book_'
 
+        # Wired URL but it is the real world
         if year == 2018:
             yearurl = ''
         else:
             yearurl = str(year) + '/'
 
         if type == 'house':
-            file_url = urllib.parse.urljoin(yearurl, key_word + typedict[type] + str(region) + extention)
+            file_path = urllib.parse.urljoin(yearurl, key_word + self.__typedict__[type] + str(region) + extention)
         elif type == 'condo':
-            file_url = urllib.parse.urljoin(yearurl, key_word + typedict[type] + extention)
-        file_full_url = urllib.parse.urljoin(base_url, str(file_url))
+            file_path = urllib.parse.urljoin(yearurl, key_word + self.__typedict__[type] + extention)
+        file_full_url = urllib.parse.urljoin(base_url, str(file_path))
         return file_full_url
-    
+
+    def pdf_filename(self, year=2012, type='house', region=1):
+        if type == 'house':
+            regionstr = str(region)
+        elif type == 'condo':
+            regionstr = ''
+        file_name = 'sales_book_' + self.__typedict__[type] + regionstr + '.pdf'
+        file_path = pathlib.Path(str(year)) / str(file_name)
+        return file_path
+
     def _download_file(self, file_url, file_name=None, save_sub_path=None):
         '''Download a single file'''
 
@@ -68,9 +84,9 @@ class wpgdata_cfg():
 
         if self.data_save_path is not None:
             fn = self.data_save_path / save_sub_path / file_name
-        
+
         pathlib.Path(self.data_save_path / save_sub_path).mkdir(parents=True, exist_ok=True)
-        
+
         if fn.exists():
             logger.info(f'The file {fn} already exists, skip downloading!')
         else:
@@ -80,15 +96,41 @@ class wpgdata_cfg():
                 return filename, headers
             except URLError as e:
                 raise RuntimeError("Failed to download '{}'. '{}'".format(file_url, e.reason))
-        
+
     def _test_url(self, url):
         result = urllib.parse.urlparse(url)
         return result
 
-    def data_auto_load(self):
-        '''Check if raw data is downloaded, auto load if found.'''
-        pass
+    def pdf_to_str(self, fpath):
+        data_whole = []
+        with pdfplumber.open(fpath) as pdf:
+            total_pages = len(pdf.pages)
+            for n in np.arange(6, total_pages):
+                # print(n)
+                pagestr = pdf.pages[n].extract_text(layout=True, x_tolerance=1, x_density=3)
+                print(pagestr)
+                # sa = loop_over_lanes(pagestr)
+                data_whole.append([pagestr])
+        return data_whole
 
-    def data_validator(self):
-        '''Check if raw data is downloaded, auto load if found.'''
-        pass
+    def data_validator(self, type='PDF'):
+        '''
+        Check if raw data is downloaded or interpreted.
+
+        Return: a boolean value.
+            Ture: dataset is complete.
+            False: dataset is not complete.
+        '''
+        result = True
+        if self.data_save_path.exists():
+            for year in self.__years__:
+                for region in self.__regions__:
+                    fn = self.pdf_filename(year=year, type='house', region=region)
+                    if not (self.data_save_path / fn).exists():
+                        logger.info(f'file {fn} missing!')
+                        result = False
+                fn = self.pdf_filename(year=year, type='condo', region=None)
+                if not (self.data_save_path / fn).exists():
+                    logger.info(f'file {fn} missing!')
+                    result = False
+        return result
---
+++ src/wpgdata/utils.py
@@ -0,0 +1,58 @@
+import pdfplumber
+import numpy as np
+import matplotlib.pyplot as plt
+from IPython.display import display
+import pandas as pd
+
+
+def get_row_data(info_str, ind_array):
+    if info_str.isspace() or (info_str is None):
+        return None
+    ind = info_str.find('$')
+    amount = info_str[ind+1:-1]
+    last_name = clean_str(info_str[ind_array[0]:ind_array[1]])
+    first_name = clean_str(info_str[ind_array[1]:ind_array[2]])
+    position = clean_str(info_str[ind_array[2]:ind])
+    compensation = float(amount.replace(" ", "").replace(',', ''))
+    data_row = [last_name, first_name, position, compensation]
+    # print(data_row)
+    return data_row
+
+
+def process_header(title_str, header_list):
+    ind_array = []
+    for item in header_list:
+        ind = title_str.find(item)
+        ind_array.append(ind)
+    # print(ind_array)
+
+    return ind_array
+
+
+def clean_str(mystring):
+    cleanstr = ' '.join(mystring.split())
+    return cleanstr.rstrip()
+
+
+def loop_over_lanes(pagestr):
+    lines = pagestr.splitlines()
+
+    trigger = False
+    data_array = []
+    for lanes in lines:
+        my_str = lanes.lstrip() + ' '     # remove leading spaces
+
+        if ('Page' in my_str) and ('of' in my_str):
+            # reach to the end of the page, no need to record.
+            trigger = False
+
+        if trigger:
+            row = get_row_data(my_str, ind_array)
+            if row is not None:
+                data_array.append(row)
+
+        if 'Last Name' in my_str:
+            ind_array = process_header(my_str, header_list)
+            trigger = True
+
+    return data_array
