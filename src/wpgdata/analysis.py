c14dfecd7edbbb93d3867e0665a47bcb6d1f650a
Yutong Zhao <yutong.zhao@algolux.com>
2023-08-31 17:01:39 -0400

    add analysis pynb

--- src/wpgdata/analysis.py
+++ src/wpgdata/analysis.py
@@ -1,5 +1,14 @@
+import pandas as pd
+import pathlib
 
 
 class data_analyser():
+
     def __init__(self) -> None:
             self._data__ = 0
+            self.data_path = pathlib.Path().absolute().parents[1] / "Winnipeg_real_estate_open_data"
+
+    def load_data(self):
+        file = self.data_path / 'combined_data.csv'
+        data = pd.read_csv(file)
+        return data
